723907bd31fb7dd1aa5e5b46352b84b4f83c4e07
kurniawan <arnoldfox21@gmail.com>
2017-11-02 22:47:59 +0700

    version 1.0.0

---
+++ application/views/admin/layout/footer.php
@@ -0,0 +1,28 @@
+						</div>
+				      </div>
+		  			</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script src="<?php echo base_url() ?>assets/admin/assets/js/jquery-1.10.2.js"></script>
+
+<script src="<?php echo base_url() ?>assets/admin/assets/js/bootstrap.min.js"></script>
+
+<script src="<?php echo base_url() ?>assets/admin/assets/js/jquery.metisMenu.js"></script>
+
+<script src="<?php echo base_url() ?>assets/admin/assets/js/dataTables/jquery.dataTables.js"></script>
+<script src="<?php echo base_url() ?>assets/admin/assets/js/dataTables/dataTables.bootstrap.js"></script>
+<script>
+$(document).ready(function () {
+$('#dataTables-example').dataTable();
+});
+</script>
+
+<script src="<?php echo base_url() ?>assets/admin/assets/js/custom.js"></script>
+
+
+</body>
+</html>
